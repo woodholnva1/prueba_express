6a0678877a631a9f1e64cab2cc964c55fd47c39c
Freddy A. Nava Ferrer <freddyanf@gmail.com>
2022-10-14 15:43:53 -0400

    Create app plantilla Midleware

--- app.js
+++ app.js
@@ -4,13 +4,24 @@ const app = express()
 const puerto = 8000
 
 /* midleware */
-app.use(morgan('dev'))
+app.use(morgan('dev'));
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true})); /* Para Peticiones POST */
+
+
 
 /* Saludo! */ 
 var saludo = "<h1>Hola mundo!</h1>";
 
 app.get('/', (req, res) => {
   res.send(`${saludo}`)
+});
+
+
+app.post('/formulario',(req,res) =>{
+    var data = req.body;
+    res.send("Tu Mensaje es: " + data.Mensaje)
+    console.log(req.body)
 });
 
 app.listen(puerto, () => {
